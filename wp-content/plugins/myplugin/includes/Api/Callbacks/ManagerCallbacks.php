6cb53c31ec09c4279d0869a5ee6c6377e8042d53
Mia Erbus <mia.erbus@gmail.com>
2020-08-13 13:34:01 +0200

    iOS looking toggle

--- wp-content/plugins/myplugin/includes/Api/Callbacks/ManagerCallbacks.php
+++ wp-content/plugins/myplugin/includes/Api/Callbacks/ManagerCallbacks.php
@@ -25,6 +25,6 @@ class ManagerCallbacks extends BaseController
         $name = $args['label_for'];
         $classes = $args['class'];
         $checkbox = get_option($name);
-        echo '<input type="checkbox" name="' . $name . '" value="0" class="' . $classes . '" ' . ($checkbox ? 'checked' : '') . '>';
+        echo '<div class="' . $classes . '"><input type="checkbox" id="' . $name . '" name="' . $name . '" value="0"' . ($checkbox ? 'checked' : '') . '><label for=' . $name . '><div></div></label></div>';
     }
 }
