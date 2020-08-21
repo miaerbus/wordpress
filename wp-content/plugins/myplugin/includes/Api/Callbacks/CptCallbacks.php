39eebfe0e700e00b5e3a64a8ea80bc0031084d0e
Mia Erbus <mia.erbus@gmail.com>
2020-08-21 18:00:28 +0200

    store arrays in wp_options

--- wp-content/plugins/myplugin/includes/Api/Callbacks/CptCallbacks.php
+++ wp-content/plugins/myplugin/includes/Api/Callbacks/CptCallbacks.php
@@ -13,30 +13,36 @@ class CptCallbacks
         echo 'Create as Many Custom Post Types as You Want';
     }
 
-    public function cptSanitize( $input )
+    public function cptSanitize($input)
     {
-        return $input;
+        $output = get_option('my_plugin_cpt');
+
+        if (empty($output)) {
+            // first cpt save, as $output is empty single array
+            $output = array($input['post_type'] => $input);
+        } else {
+            foreach ($output as $key => $value) {
+                if ($input['post_type'] === $key) {
+                    $output[$key] = $input;
+                } else {
+                    $output[$input['post_type']] = $input;
+                }
+            }
+        }
+        return $output;
     }
 
-    public function textField( $args )
+    public function textField($args)
     {
         $name = $args['label_for'];
-        $option_name = $args['option_name'];
-        $placeholder = $args['placeholder'];
-        $input = get_option($option_name);
-        $value = isset($input[$name]) ? $input[$name] : '';
 
-        echo '<input type="text" class="regular-text" id="' . $name . '" name="' . $option_name . '[' . $name . ']" value="' . $value . '" placeholder="' . $placeholder . '">';
+        echo '<input type="text" class="regular-text" id="' . $name . '" name="' . $args['option_name'] . '[' . $name . ']" value="" placeholder="' . $args['placeholder'] . '">';
     }
 
-    public function checkboxField( $args )
+    public function checkboxField($args)
     {
         $name = $args['label_for'];
-        $classes = $args['class'];
-        $option_name = $args['option_name'];
-        $checkbox = get_option($option_name);
-        $checked = isset($checkbox[$name]) ? 'checked' : '';
 
-        echo '<div class="' . $classes . '"><input type="checkbox" id="' . $name . '" name="' . $option_name . '[' . $name . ']" value="1"' . $checked . '><label for=' . $name . '><div></div></label></div>';
+        echo '<div class="' . $args['class'] . '"><input type="checkbox" id="' . $name . '" name="' . $args['option_name'] . '[' . $name . ']" value="1"><label for=' . $name . '><div></div></label></div>';
     }
 }
